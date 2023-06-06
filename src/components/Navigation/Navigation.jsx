109e643d3ba7b335988622656fffb324771e4e2e
TarasTikan <tikantaras1@gmail.com>
2023-06-07 00:59:53 +0300

    add ada

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 // import { useSelector } from 'react-redux';
 // import { selectIsLoggein } from 'redux/auth/selectors';
+import { UserIcon } from 'images/icons/UserIcon';
 import {
-  StyledLink,
   WrapAuth,
   BtnLogout,
   TitleUser,
@@ -9,33 +9,160 @@ import {
   Nav,
   WrapIcon,
   HeroTitle,
-  IconHero,
+  StyledLink,
+  LinkWrapper,
+  MobileMenu,
+  MobileContainer,
+  ButtonClose,
+  NavWrappText,
 } from './Navigation.styled';
 // import { NavLink } from 'react-router-dom';
+import { Calendar } from 'images/icons/Calendar';
+import { useState } from 'react';
+import { LogoutIcon } from 'images/icons/LogoutIcon';
+import goose1pxDesck from 'images/imagesGoose/goose1pxDescktop.png'
+import goose2pxDesck from 'images/imagesGoose/goose2pxDescktop.png';
+import goose1pxTable from 'images/imagesGoose/goose1pxTable.png';
+import goose2pxTable from 'images/imagesGoose/goose2pxTable.png';
+import goose1pxMobile from 'images/imagesGoose/goose1pxMobile.png';
+import goose2pxMobile from 'images/imagesGoose/goose2pxMobile.png';
+import { Close } from '@mui/icons-material';
 export const Navigation = () => {
+   const [activeBlock, setActiveBlock] = useState(null);
 
+   const handleBlockClick = block => {
+     setActiveBlock(block);
+   };
   return (
     <nav>
+      <MobileMenu>
+        <MobileContainer>
+          <WrapIcon>
+            <picture>
+              <source
+                srcset={`${goose1pxTable},${goose2pxTable}`}
+                media="(min-width:768px)"
+              />
+              <source
+                srcset={`${goose1pxMobile},${goose2pxMobile}`}
+                media="(max-width:768px)"
+              />
+              <img
+                src={goose1pxMobile}
+                alt="Goose"
+                width="36"
+                height="35"
+              />
+            </picture>
+            <HeroTitle>GooseTrack</HeroTitle>
+            <ButtonClose><Close/></ButtonClose>
+          </WrapIcon>
+          <NavWrappText>
+          <TitleUser>User Panel</TitleUser>
+            <Nav>
+              <NavList>
+                <li>
+                  <LinkWrapper
+                    active={activeBlock === 'block1'}
+                    onClick={() => handleBlockClick('block1')}
+                  >
+                    <UserIcon />
+                    <StyledLink
+                      to="/contacts"
+                      active={activeBlock === 'block1'}
+                      onClick={() => handleBlockClick('block1')}
+                    >
+                      My account
+                    </StyledLink>
+                  </LinkWrapper>
+                </li>
+                <li>
+                  <LinkWrapper
+                    active={activeBlock === 'block2'}
+                    onClick={() => handleBlockClick('block2')}
+                  >
+                    <Calendar />
+                    <StyledLink
+                      to="/"
+                      active={activeBlock === 'block2'}
+                      onClick={() => handleBlockClick('block2')}
+                    >
+                      Calendar
+                    </StyledLink>
+                  </LinkWrapper>
+                </li>
+              </NavList>
+            </Nav>
+          </NavWrappText>
+          <BtnLogout>
+            Log out <LogoutIcon />
+          </BtnLogout>
+        </MobileContainer>
+      </MobileMenu>
       <WrapAuth>
         <WrapIcon>
-          <IconHero />
+          {/* <img src={`${goose1pxDesck}`} alt="goose"></img> */}
+          <picture>
+            <source
+              srcset={`${goose1pxDesck},${goose2pxDesck}`}
+              media="(min-width:1200px)"
+            />
+            <source
+              srcset={`${goose1pxTable},${goose2pxTable}`}
+              media="(min-width:768px)"
+            />
+            <source
+              srcset={`${goose1pxMobile},${goose2pxMobile}`}
+              media="(max-width:768px)"
+            />
+            <img
+              src={goose1pxMobile}
+              alt="Goose"
+              width="36"
+              height="35"
+            />
+          </picture>
           <HeroTitle>GooseTrack</HeroTitle>
         </WrapIcon>
         <TitleUser>User Panel</TitleUser>
         <Nav>
           <NavList>
             <li>
-              <StyledLink to="/">My account</StyledLink>
+              <LinkWrapper
+                active={activeBlock === 'block1'}
+                onClick={() => handleBlockClick('block1')}
+              >
+                <UserIcon />
+                <StyledLink
+                  to="/contacts"
+                  active={activeBlock === 'block1'}
+                  onClick={() => handleBlockClick('block1')}
+                >
+                  My account
+                </StyledLink>
+              </LinkWrapper>
             </li>
             <li>
-              <StyledLink to="/contacts">My contacts</StyledLink>
+              <LinkWrapper
+                active={activeBlock === 'block2'}
+                onClick={() => handleBlockClick('block2')}
+              >
+                <Calendar />
+                <StyledLink
+                  to="/"
+                  active={activeBlock === 'block2'}
+                  onClick={() => handleBlockClick('block2')}
+                >
+                  Calendar
+                </StyledLink>
+              </LinkWrapper>
             </li>
           </NavList>
         </Nav>
-        <BtnLogout>Log out</BtnLogout>
+        <BtnLogout>
+          Log out <LogoutIcon />
+        </BtnLogout>
       </WrapAuth>
-      {/* <StyledLink to="/">Home</StyledLink>
-      {isLoggein && <StyledLink to="/contacts">Contacts</StyledLink>} */}
     </nav>
   );
 };
--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-
+// import Icons from '../../images/icons/grayicon.svg';
 // export const StyledLink = styled(NavLink)`
 //   padding: 8px 16px;
 //   border-radius: 4px;
@@ -25,51 +25,25 @@ export const WrapAuth = styled.div`
 `;
 
 
-export const BtnLogout = styled.button`
-  box-shadow: 4px 2px 16px rgba(136, 165, 191, 0.48);
-  border-radius: 16px;
-  width: 141px;
-  height: 56px;
-  background-color: #3e85f3;
-  border: none;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 24px;
-  color: #ffff;
-`;
-
-
 export const TitleUser = styled.p`
   color: rgba(52, 52, 52, 0.5);
   font-weight: 600;
-  font-size: 14px;
-  line-height: 17px;
+  font-size: 12px;
+  line-height: 15px;
   margin-top: 32px;
-  margin-bottom: 15px;
-`;
-
-export const StyledLink = styled(NavLink)`
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 19px;
-  text-decoration: none;
-  padding: 16px 165px 16px 20px;
-  color: rgba(52, 52, 52, 0.5);
-  &.active {
-    color: #3e85f3;
-    padding: 16px 165px 16px 20px;
-    background-color: #e3f3ff;
-    border-radius: 8px;
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+    line-height: 17px;
   }
 `;
 
   export const NavList = styled.ul`
     display: flex;
     flex-direction: column;
-    gap: 34px;
+    gap: 16px;
     list-style: none;
     padding-left: 0;
-    margin-top: 32px;
+    margin-top: 10px;
   `;
 
  export const Nav = styled.nav`
@@ -77,24 +51,152 @@ export const StyledLink = styled(NavLink)`
    flex-direction: column;
    margin-bottom: auto;
    
- `;
+   `;
 
  export const WrapIcon = styled.div`
    display: flex;
+   align-items: center;
+   gap: 6px;
  `;
 
- export const HeroTitle = styled.h1`
-   font-weight: 400;
-   font-size: 24px;
-   line-height: 24px;
-   text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
-     0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
-   color: #3e85f3;
- `;
-
+     
+export const LinkWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  padding-top: 11px;
+  padding-bottom: 18px;
+  padding-right: 81px;
+  width: 185px;
+  height: 40px;
+  border-radius: 8px;
+  gap: 5px;
+  background-color: ${({ active }) => (active ? '#e3f3ff' : 'transparent')};
+  color: ${({ active }) => (active ? '#fff' : '#000')};
+  stroke: ${({ active }) => (active ? '#3e85f3' : '#34343480')};
+  transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    background-color: '#e3f3ff';
+    color: '#3e85f3';
+    transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+  svg {
+    width: 15px;
+    height: 16.67px;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    line-height: 19px;
+    padding-left: 22px;
+    padding-top: 18px;
+    padding-bottom: 18px;
+    padding-right: 96px;
+  }
+`;
  
- export const IconHero = styled.div`
-   height: 68px;
-   width: 71px;
-   background-color: #3e85f3;
- `;
+  export const StyledLink = styled(NavLink)`
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 17px;
+    text-decoration: none;
+    transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    color: ${({ active }) => (active ? '#3e85f3' : 'rgba(52, 52, 52, 0.5)')};
+    &:hover {
+      color: '#3e85f3';
+    }
+    @media screen and (min-width: 768px) {
+      font-size: 16px;
+      line-height: 19px;
+    }
+  `;
+
+  export const BtnLogout = styled.button`
+    box-shadow: 4px 2px 16px rgba(136, 165, 191, 0.48);
+    border-radius: 16px;
+    width: 141px;
+    height: 56px;
+    background-color: #3e85f3;
+    border: none;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+    color: #ffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    stroke: '#fff';
+    transition: stroke 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    &:hover {
+      background-color: '#000';
+    }
+    svg {
+      margin-left: 13px;
+      width: 13px;
+      height: 13px;
+    }
+    svg:hover {
+      stroke: '#0000';
+    }
+    @media screen and (min-width: 768px) {
+      font-size: 18px;
+      line-height: 24px;
+    }
+  `;
+export const MobileMenu = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+  z-index: 1;
+  /* padding-top: 24px;
+  padding-bottom: 24px;
+  padding-left: 20px;
+  padding-right: 26px; */
+  padding-top: 24px;
+  padding-bottom: 24px;
+  padding-left: 20px;
+  padding-right: 26px;
+`;
+
+export const MobileContainer = styled.div`
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  display: flex;
+  `;
+
+
+export const ButtonClose = styled.button`
+  background-color: transparent;
+  border: none;
+  display: block;
+  margin-left: auto;
+  cursor: pointer;
+  transition: fill 250ms cubic-bezier(0.4, 0, 0.2, 1);
+`;
+
+  export const HeroTitle = styled.h1`
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 22px;
+    color: #3e85f3;
+    text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
+      0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
+    @media screen and (min-width: 768px) {
+      font-size: 18px;
+      line-height: 24px;
+    }
+    /* font-weight: 400;
+    font-size: 24px;
+    line-height: 24px;
+    text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
+      0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
+    color: #3e85f3; */
+  `;
+
+  export const NavWrappText = styled.div`
+   margin-bottom: auto;
+  `;
